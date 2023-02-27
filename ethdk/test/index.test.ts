f583285ddb64cbca0bd06b83d04f1df670f6631f
Blake Duncan <blakecduncan@gmail.com>
2023-02-27 16:09:12 +0000

    Fix eslint settings

--- ethdk/test/index.test.ts
+++ ethdk/test/index.test.ts
@@ -1,11 +1,11 @@
-import { generatePrivateKey } from '../src/Ethdk';
-import { expect } from 'chai';
+import { generatePrivateKey } from '../src/Ethdk'
+import { expect } from 'chai'
 
 describe('Ethdk', () => {
   it('should create a bls private key', async () => {
-    const privateKey = await generatePrivateKey('bls');
+    const privateKey = await generatePrivateKey('bls')
     // Private key should be a hex string of length 66
-    const privateKeyRegex = /^0x[0-9a-fA-F]{64}$/;
-    expect(privateKey).to.match(privateKeyRegex);
-  });
-});
+    const privateKeyRegex = /^0x[0-9a-fA-F]{64}$/
+    expect(privateKey).to.match(privateKeyRegex)
+  })
+})
