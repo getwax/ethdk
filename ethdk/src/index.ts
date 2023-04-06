17bc3786488e93968558183b08cbd35601b20bc4
JohnGuilding <jwguilding@gmail.com>
2023-04-06 13:04:36 +0100

    Export ExternallyOwnedAccount

--- ethdk/src/index.ts
+++ ethdk/src/index.ts
@@ -1,6 +1,15 @@
 import { createAccount } from './Ethdk'
 import BlsAccount from './Bls/BlsAccount'
 import BlsTransaction from './Bls/BlsTransaction'
+import ExternallyOwnedAccount from './EOA/ExternallyOwnedAccount'
+import EoaTransaction from './EOA/EoaTransaction'
 import * as networks from './Networks'
 
-export { createAccount, BlsAccount, BlsTransaction, networks }
+export {
+  createAccount,
+  BlsAccount,
+  BlsTransaction,
+  ExternallyOwnedAccount,
+  EoaTransaction,
+  networks,
+}
